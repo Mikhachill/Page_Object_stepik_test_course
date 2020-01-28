6ac03b4992a7e0d86e978b83730ad96f6ca3373a
Mikhachill <horror200687@mail.ru>
2020-01-29 02:19:18 +0600

    Checking code

--- test_main_page.py
+++ test_main_page.py
@@ -9,12 +9,10 @@ class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self,browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-        page.open()                      #cd открываем страницу
+        page.open()                      # открываем страницу
         page.go_to_login_page()
-        login_page=LoginPage(browser, browser.current_url) # если комментить эту строку то надо править main_page.py
-        #что бы работал первый подход к переходу между страницами
-        login_page.should_be_login_page() #выполняем метод страницы - переходим на страницу логина
-      
+        login_page=LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
         
     def test_guest_should_see_login_link(self,browser):
--- test_product_page.py
+++ test_product_page.py
@@ -23,7 +23,8 @@ class TestUserAddToBasketFromProductPage():
         page=ProductPage(browser, link)
         page.open()
         page.should_not_to_be_present()
-                    
+
+    @pytest.mark.need_review                
     def test_user_can_add_product_to_basket(self,browser):
         link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6"
         page=ProductPage(browser,link)
@@ -38,45 +39,41 @@ class TestUserAddToBasketFromProductPage():
 #Открываем страницу товара 
 #Добавляем товар в корзину 
 #Проверяем, что нет сообщения об успехе с помощью is_not_element_present
-##@pytest.mark.xfail(reason="just fail test")
-##def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-##    link="http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
-##    page=ProductPage(browser, link)
-##    page.open()
-##
-##    page.should_be_clickable_basket_button()
-##    page.should_not_to_be_present()
-##    
-##    
+@pytest.mark.xfail(reason="just fail test")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link="http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    page=ProductPage(browser, link)
+    page.open()
 
+    page.should_be_clickable_basket_button()
+    page.should_not_to_be_present()
     
-
+    
 #Открываем страницу товара
 #Добавляем товар в корзину
 #Проверяем, что нет сообщения об успехе с помощью is_disappeared
-##@pytest.mark.xfail(reason="just fail test")
-##def test_message_disappeared_after_adding_product_to_basket(browser):
-##    link="http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
-##    page=ProductPage(browser, link)
-##    page.open()
-##
-##    page.should_be_clickable_basket_button()
-##    
-##    page.should_be_disappeared()
-##
-##def test_guest_should_see_login_link_on_product_page(browser):
-##    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-##    page = ProductPage(browser, link)
-##    page.open()
-##    page.should_be_login_link()
+@pytest.mark.xfail(reason="just fail test")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link="http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    page=ProductPage(browser, link)
+    page.open()
+    page.should_be_clickable_basket_button()  
+    page.should_be_disappeared()
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.need_review
 def test_quest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page= ProductPage(browser,link)
     page.open()
     page.go_to_login_page()
 
-###Открываем страницу товара 
+#Открываем страницу товара 
 #Проверяем, что нет сообщения об успехе с помощью is_not_element_present 
 def test_guest_cant_see_success_message(browser):
     link="http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
@@ -88,31 +85,28 @@ def test_guest_cant_see_success_message(browser):
     
 xfile = 7
 mask = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer'
-links = [mask+str(i) for i in range(10) if i != xfile] #range10 # отсекаем из проверки страницу с ошибкой генератор списка что бысоздать все страницы от 0 до 9
-xlink = pytest.param(mask+str(xfile), marks=pytest.mark.xfail(reason="mistake on page")) #
+links = [mask+str(i) for i in range(10) if i != xfile] #range10  отсекаем из проверки страницу с ошибкой генератор списка что бы создать все страницы от 0 до 9
+xlink = pytest.param(mask+str(xfile), marks=pytest.mark.xfail(reason="mistake on page")) 
 links.insert(xfile, xlink)
 
 @pytest.mark.parametrize('link', links)
-                         
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser,link):
-    #link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6"
     page=ProductPage(browser,link)
     page.open()
     page.should_be_basket_button_in_product_page()
     page.should_be_clickable_basket_button()
     page.solve_quiz_and_get_code()
-
-   # page.should_be_disappeared() 
     page.should_be_message_about_adding()
     page.should_be_message_basket_total()
 
    
 
-
-##Гость открывает страницу товара
-##Переходит в корзину по кнопке в шапке 
-##Ожидаем, что в корзине нет товаров
-##Ожидаем, что есть текст о том что корзина пуста 
+#Гость открывает страницу товара
+#Переходит в корзину по кнопке в шапке 
+#Ожидаем, что в корзине нет товаров
+#Ожидаем, что есть текст о том что корзина пуста
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link ="http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
     page =BasketPage(browser,link)
